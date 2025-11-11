71b6f4716cb38dca156e4324c18eee15b815dbf1
wadsonushemakota-tech <wadsonushemakota@gmail.com>
2025-11-11 22:58:25 +0200

    Index: extend ENACTUS slider to 12 images (512)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,6 +13,14 @@ import img1 from "@/assets/1.jpg";
 import img2 from "@/assets/2.jpg";
 import img3 from "@/assets/3.jpg";
 import img4 from "@/assets/4.jpg";
+import img5 from "@/assets/5.jpg";
+import img6 from "@/assets/6.jpg";
+import img7 from "@/assets/7.jpg";
+import img8 from "@/assets/8.jpg";
+import img9 from "@/assets/9.jpg";
+import img10 from "@/assets/10.jpg";
+import img11 from "@/assets/11.jpg";
+import img12 from "@/assets/12.jpg";
 
 const Index = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -20,7 +28,11 @@ const Index = () => {
 
   // ENACTUS left-side carousel (all images on the left)
   const [enactusIndex, setEnactusIndex] = useState(0);
-  const enactusImages = [img1, img2, img3, img4];
+  const enactusImages = [
+    img1, img2, img3, img4,
+    img5, img6, img7, img8,
+    img9, img10, img11, img12,
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
